3d76c25dc1dab8d75a84e2924c342f509c71bde1
Levis155 <levismbui@gmail.com>
2025-04-22 09:37:33 +0300

    store supabase url and api key in .env

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,3 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient("https://dccvrcjnjrrlchzahaiz.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjY3ZyY2puanJybGNoemFoYWl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxMjU1ODMsImV4cCI6MjA2MDcwMTU4M30.-HYZ8MG-Ctr9hsezpSELzl18TRV6c6ucaEzdw3r5FoM");
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
